36a44596a4c7bd8a94c8f65146513c86d64dc939
brittyu <yu.britt@gmail.com>
2014-05-15 17:54:10 +0800

    delete some files

--- application/views/template/header.php
+++
@@ -1,8 +0,0 @@
-<header id="header" class="navbar navbar-fixed-top">
-	<div class="container">
-		<div class="navbar">
-			<a class="navbar-brand" href="#">Title</a>
-			<?php echo $nav?>
-		</div>
-	</div>
-</header>
--- application/views/template/nav.php
+++
@@ -1,8 +0,0 @@
-<ul class="nav navbar-nav">
-	<li class="<?php echo isActive($pageName,"home")?>"><a href="<?php echo  base_url()?>">Home</a></li>
-	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()?>">Link</a></li>
-	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()?>">Link</a></li>
-	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()?>">Link</a></li>
-	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()?>">Link</a></li>
-	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()?>">Link</a></li>
-</ul>
